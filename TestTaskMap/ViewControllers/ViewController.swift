b33f33f6930ebdeec9e53f130e93ce4f35407185
seleza1 <koposov.serezha@gmail.com>
2022-11-22 13:34:55 +0300

    add Buttons

--- TestTaskMap/ViewControllers/ViewController.swift
+++ TestTaskMap/ViewControllers/ViewController.swift
@@ -16,10 +16,62 @@ class ViewController: UIViewController {
         mapView.translatesAutoresizingMaskIntoConstraints = false
         return mapView
     }()
-
+    
+    let addAdressButton: UIButton = {
+        let button = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setTitle("Add adress", for: .normal)
+        button.titleLabel?.numberOfLines = 0
+        button.backgroundColor = .red
+        button.contentHorizontalAlignment = .center
+        button.layer.cornerRadius = 35
+        return button
+    }()
+    
+    let roadButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("Road", for: .normal)
+        button.backgroundColor = .green
+        button.layer.cornerRadius = 15
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
+    let resetButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("Reset", for: .normal)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.layer.cornerRadius = 15
+        button.backgroundColor = .red
+        
+        return button
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         setConstraints()
+        
+        addAdressButton.addTarget(self,action: #selector(addAdressButtonTapped), for: .touchUpInside)
+        roadButton.addTarget(self, action: #selector(roadButtonTapped), for: .touchUpInside)
+        resetButton.addTarget(self, action: #selector(resetButtonTapped), for: .touchUpInside)
+        
+    }
+    
+    @objc func addAdressButtonTapped() {
+        print("addTapped")
+        
+        
+    }
+    
+    @objc func roadButtonTapped() {
+        print("tappedRoad")
+        
+    }
+    
+    @objc func resetButtonTapped() {
+        print("tappedReset")
+        
     }
 }
 
@@ -34,5 +86,38 @@ extension ViewController {
             mapView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: 0),
             mapView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: 0)
         ])
+        
+        mapView.addSubview(addAdressButton)
+        NSLayoutConstraint.activate([
+            addAdressButton.topAnchor.constraint(equalTo: mapView.topAnchor, constant: 50),
+            addAdressButton.trailingAnchor.constraint(equalTo: mapView.trailingAnchor, constant: -20),
+            addAdressButton.heightAnchor.constraint(equalToConstant: 70),
+            addAdressButton.widthAnchor.constraint(equalToConstant: 70)
+        
+        
+        ])
+        
+        mapView.addSubview(roadButton)
+        NSLayoutConstraint.activate([
+            roadButton.leadingAnchor.constraint(equalTo: mapView.leadingAnchor, constant: 20),
+            roadButton.bottomAnchor.constraint(equalTo: mapView.bottomAnchor, constant: -30),
+            roadButton.heightAnchor.constraint(equalToConstant: 50),
+            roadButton.widthAnchor.constraint(equalToConstant: 100)
+        
+        
+        ])
+        
+        mapView.addSubview(resetButton)
+        NSLayoutConstraint.activate([
+            resetButton.trailingAnchor.constraint(equalTo: mapView.trailingAnchor, constant: -20),
+            resetButton.bottomAnchor.constraint(equalTo: mapView.bottomAnchor, constant: -30),
+            resetButton.heightAnchor.constraint(equalToConstant: 50),
+            resetButton.widthAnchor.constraint(equalToConstant: 100)
+        
+        
+        ])
+
+        
+        
     }
 }
